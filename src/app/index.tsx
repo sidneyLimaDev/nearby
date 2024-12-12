9cd69bfb7ce7c6078dacec229f4a531931b34695
Sidney Lima <sidneylimadev@gmail.com>
2024-12-12 12:37:01 -0300

    Feat: Create category structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
+import { View } from "react-native";
+import { router } from "expo-router";
+
 import { Steps } from "@/components/steps";
 import { Welcome } from "@/components/welcome";
-import { View } from "react-native";
 import { Button } from "@/components/button";
 
 export default function Index() {
@@ -8,7 +10,7 @@ export default function Index() {
        <View style={{ flex:1, padding: 40, gap: 40}}>
         <Welcome/>
         <Steps/>
-        <Button >
+        <Button onPress={() => router.push("/home")}>
             <Button.Title>Começar</Button.Title>
         </Button>
        </View>   
