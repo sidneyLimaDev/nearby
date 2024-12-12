3cf8fb7ffd17b3b984c856acfa4c2388d24899db
Sidney Lima <sidneylimadev@gmail.com>
2024-12-12 00:03:02 -0300

    chore: Create project and add loading

---
+++ src/app/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router"
+import { colors } from "@/styles/theme"
+import { useFonts, Rubik_400Regular, Rubik_600SemiBold, Rubik_500Medium, Rubik_700Bold } from "@expo-google-fonts/rubik"
+import { Loading } from "@/components/loading"
+
+export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Rubik_400Regular, 
+        Rubik_600SemiBold, 
+        Rubik_500Medium, 
+        Rubik_700Bold,
+    })
+
+    if (!fontsLoaded) {
+        return <Loading />
+    }
+    
+    return (
+    <Stack screenOptions={{ 
+        headerShown: false, 
+        contentStyle: { backgroundColor: colors.gray[100] }
+    }} 
+    />
+)
+}
